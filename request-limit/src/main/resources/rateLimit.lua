d6106696c77e818581eebab954c50f17c9f58fed
changwen <983081622@qq.com>
2019-01-10 12:04:21 +0800

    修复限流脚本bug

--- request-limit/src/main/resources/rateLimit.lua
+++ request-limit/src/main/resources/rateLimit.lua
@@ -1,10 +1,13 @@
 local key = "rate.limit:" .. KEYS[1] --限流KEY（一秒一个）
 local limit = tonumber(ARGV[1]) --限流大小
+local limitTime = tonumber(ARGV[2]) --限流时间
 local current = tonumber(redis.call('get', key) or "0")
-if current + 1 > limit then --如果超出限流大小
+if current + 1 > limit then
+    --如果超出限流大小
     return 0
-else --请求数+1，并设置2秒过期
-    redis.call("INCRBY", key,"1")
-    redis.call("expire", key,"2")
-    return 1
+else
+    --请求数+1，并设置2秒过期
+    redis.call("INCRBY", key, "1")
+    redis.call("expire", key, limitTime)
+    return current + 1
 end
